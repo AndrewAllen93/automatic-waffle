1b93ed3ecf842f625af6b9e5b6fed605e5f1e5cc
Andrew Allen <allenkandrew@gmail.com>
2023-11-06 21:01:23 -0500

    added search history to html and changed javascript to make each item clickable

--- assets/script.js
+++ assets/script.js
@@ -21,55 +21,75 @@ function saveSearch(city) {
 
 function updateSearchHistory() {
   const searchHistory = JSON.parse(localStorage.getItem("searches")) || [];
-  const searchHistoryElement = document.querySelector(".search-history");
+  const searchItemsContainer = document.querySelector(".search-items");
+  searchItemsContainer.innerHTML = ""; // Clear existing items
 
-  if (searchHistoryElement) {
-    searchHistoryElement.innerHTML = "";
+  searchHistory.forEach((search, index) => {
+    const searchItem = document.createElement("div");
+    const searchLink = document.createElement("a");
 
-    searchHistory.forEach((search, index) => {
-      const searchItem = document.createElement("div");
-      searchItem.textContent = search;
-      searchHistoryElement.appendChild(searchItem);
+    searchLink.href = "#"; // Add a placeholder URL
+    searchLink.textContent = search;
+    searchLink.addEventListener("click", (event) => {
+      event.preventDefault(); // Prevent the default link behavior
+      // Handle the click event here, e.g., perform a new search with the clicked term
+      fetchWeatherData(search);
     });
-  }
+
+    searchItem.appendChild(searchLink);
+    searchItemsContainer.appendChild(searchItem);
+  });
 }
+
 
 window.addEventListener("load", updateSearchHistory);
 
 function fetchWeatherData(city) {
-  fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=imperial`)
+  fetch(
+    `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=imperial`
+  )
     .then((response) => response.json())
     .then((data) => {
       const emojiMap = {
-        '01d': '☀️', // Clear sky (day)
-        '01n': '🌙', // Clear sky (night)
-        '02d': '🌤️', // Few clouds (day)
-        '02n': '🌤️', // Few clouds (night)
-        '03d': '🌥️', // Scattered clouds (day)
-        '03n': '🌥️', // Scattered clouds (night)
-        '04d': '☁️', // Broken clouds (day)
-        '04n': '☁️', // Broken clouds (night)
-        '09d': '🌧️', // Rain (day)
-        '09n': '🌧️', // Rain (night)
-        '10d': '🌦️', // Rain and few clouds (day)
-        '10n': '🌦️', // Rain and few clouds (night)
-        '11d': '⛈️', // Thunderstorm (day)
-        '11n': '⛈️', // Thunderstorm (night)
-        '13d': '🌨️', // Snow (day)
-        '13n': '🌨️', // Snow (night)
-        '50d': '🌫️', // Mist (day)
-        '50n': '🌫️', // Mist (night)
+        "01d": "☀️", // Clear sky (day)
+        "01n": "🌙", // Clear sky (night)
+        "02d": "🌤️", // Few clouds (day)
+        "02n": "🌤️", // Few clouds (night)
+        "03d": "🌥️", // Scattered clouds (day)
+        "03n": "🌥️", // Scattered clouds (night)
+        "04d": "☁️", // Broken clouds (day)
+        "04n": "☁️", // Broken clouds (night)
+        "09d": "🌧️", // Rain (day)
+        "09n": "🌧️", // Rain (night)
+        "10d": "🌦️", // Rain and few clouds (day)
+        "10n": "🌦️", // Rain and few clouds (night)
+        "11d": "⛈️", // Thunderstorm (day)
+        "11n": "⛈️", // Thunderstorm (night)
+        "13d": "🌨️", // Snow (day)
+        "13n": "🌨️", // Snow (night)
+        "50d": "🌫️", // Mist (day)
+        "50n": "🌫️", // Mist (night)
         // You can add more mappings for various weather conditions here
       };
 
       const weatherEmoji = emojiMap[data.weather[0].icon];
 
-      document.querySelector(".current-weather h2").textContent = `${weatherEmoji} ${data.name}, ${data.sys.country}`;
-      document.querySelector(".current-weather h6:nth-of-type(1)").textContent = `Temperature: ${data.main.temp}°F`;
-      document.querySelector(".current-weather h6:nth-of-type(2)").textContent = `Wind: ${data.wind.speed} M/S`;
-      document.querySelector(".current-weather h6:nth-of-type(3)").textContent = `Humidity: ${data.main.humidity}%`;
+      document.querySelector(
+        ".current-weather h2"
+      ).textContent = `${weatherEmoji} ${data.name}, ${data.sys.country}`;
+      document.querySelector(
+        ".current-weather h6:nth-of-type(1)"
+      ).textContent = `Temperature: ${data.main.temp}°F`;
+      document.querySelector(
+        ".current-weather h6:nth-of-type(2)"
+      ).textContent = `Wind: ${data.wind.speed} M/S`;
+      document.querySelector(
+        ".current-weather h6:nth-of-type(3)"
+      ).textContent = `Humidity: ${data.main.humidity}%`;
 
-      return fetch(`https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${apiKey}&units=imperial`);
+      return fetch(
+        `https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${apiKey}&units=imperial`
+      );
     })
     .then((response) => response.json())
     .then((data) => {
@@ -83,35 +103,42 @@ function fetchWeatherData(city) {
         const humidity = day.main.humidity;
 
         const emojiMap = {
-          '01d': '☀️', // Clear sky (day)
-          '01n': '🌙', // Clear sky (night)
-          '02d': '🌤️', // Few clouds (day)
-          '02n': '🌤️', // Few clouds (night)
-          '03d': '🌥️', // Scattered clouds (day)
-          '03n': '🌥️', // Scattered clouds (night)
-          '04d': '☁️', // Broken clouds (day)
-          '04n': '☁️', // Broken clouds (night)
-          '09d': '🌧️', // Rain (day)
-          '09n': '🌧️', // Rain (night)
-          '10d': '🌦️', // Rain and few clouds (day)
-          '10n': '🌦️', // Rain and few clouds (night)
-          '11d': '⛈️', // Thunderstorm (day)
-          '11n': '⛈️', // Thunderstorm (night)
-          '13d': '🌨️', // Snow (day)
-          '13n': '🌨️', // Snow (night)
-          '50d': '🌫️', // Mist (day)
-          '50n': '🌫️', // Mist (night)
+          "01d": "☀️", // Clear sky (day)
+          "01n": "🌙", // Clear sky (night)
+          "02d": "🌤️", // Few clouds (day)
+          "02n": "🌤️", // Few clouds (night)
+          "03d": "🌥️", // Scattered clouds (day)
+          "03n": "🌥️", // Scattered clouds (night)
+          "04d": "☁️", // Broken clouds (day)
+          "04n": "☁️", // Broken clouds (night)
+          "09d": "🌧️", // Rain (day)
+          "09n": "🌧️", // Rain (night)
+          "10d": "🌦️", // Rain and few clouds (day)
+          "10n": "🌦️", // Rain and few clouds (night)
+          "11d": "⛈️", // Thunderstorm (day)
+          "11n": "⛈️", // Thunderstorm (night)
+          "13d": "🌨️", // Snow (day)
+          "13n": "🌨️", // Snow (night)
+          "50d": "🌫️", // Mist (day)
+          "50n": "🌫️", // Mist (night)
           // You can add more mappings for various weather conditions here
         };
 
         const weatherEmoji = emojiMap[day.weather[0].icon];
 
-        forecastCards[i].querySelector("h3").textContent = `${weatherEmoji} ${date.toLocaleDateString()}`;
-        forecastCards[i].querySelectorAll("h6")[0].textContent = `Temp: ${temperature}°F`;
-        forecastCards[i].querySelectorAll("h6")[1].textContent = `Wind: ${wind} M/S`;
-        forecastCards[i].querySelectorAll("h6")[2].textContent = `Humidity: ${humidity}%`;
+        forecastCards[i].querySelector(
+          "h3"
+        ).textContent = `${weatherEmoji} ${date.toLocaleDateString()}`;
+        forecastCards[i].querySelectorAll(
+          "h6"
+        )[0].textContent = `Temp: ${temperature}°F`;
+        forecastCards[i].querySelectorAll(
+          "h6"
+        )[1].textContent = `Wind: ${wind} M/S`;
+        forecastCards[i].querySelectorAll(
+          "h6"
+        )[2].textContent = `Humidity: ${humidity}%`;
       }
     })
     .catch((error) => console.error("Error:", error));
 }
-
